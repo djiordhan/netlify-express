5bb706c32f0362dda960f0f45cc13ca89cedc6d2
Jordan Diamante <diamantejordan@gmail.com>
2020-08-11 16:32:33 +0800

    add post method

--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const pjson = require('../package.json');
+const bodyParser = require('body-parser');
 
 const serverless = require('serverless-http');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(cors());
 
@@ -18,8 +22,14 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.json({
+        'version': pjson.version
+    });
+});
+
+router.post('/', (req, res) => {
+    res.json({
         'version': pjson.version,
-        'hello': 'formbird'
+        'body': JSON.parse(req.body)
     });
 });
 
