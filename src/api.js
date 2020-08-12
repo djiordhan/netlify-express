84108f1d131ee311d76d4ba6bc9b4a663ec243c1
Jordan Diamante <diamantejordan@gmail.com>
2020-08-12 16:53:26 +0800

    display posted data

--- src/api.js
+++ src/api.js
@@ -20,19 +20,41 @@ app.use(function(req, res, next) {
 
 const router = express.Router();
 
+const postData = [];
+
 router.get('/', (req, res) => {
-    res.json({
-        'version': pjson.version
-    });
+
+    let tableHTML = '<table style="width: 100%"><tr><td><b>Time</b></td><td><b>Data</b></td></tr>';
+
+    for (const data of postData) {
+        tableHTML += `<tr><td>${data.date}</td><td>${JSON.stringify(data.data)}</td></tr>`;
+    }
+    tableHTML += '</table>';
+
+    res.send(`
+    <html>
+        <body>
+            ${tableHTML}
+        </body>
+    </html>
+    `);
 });
 
 router.post('/', (req, res) => {
+
+    postData.push({
+        date: new Date(),
+        data: JSON.parse(req.body)
+    });
+
     res.json({
         'version': pjson.version,
         'body': JSON.parse(req.body)
     });
 });
 
+
+app.use('/api', router);
 app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
